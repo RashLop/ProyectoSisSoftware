9ef761b2288e6662d8b9085464e426e209e69c47
RashLop <lopezmejiarasshid@outlook.com>
2025-09-09 21:08:27 -0400

    feat: Se ha agregado la funcion de calcular tarifa, te devuelve la tarifa base que son 10 bs. Se ha cambiado solo el nombre de la funcion mostrar a mostrarFechas, para mas legibilidad del codigo

--- src/ticketParking.js
+++ src/ticketParking.js
@@ -4,10 +4,13 @@ export default class ticketParking
         this.Entrada =  new Date(horaEntrada);
         this.Salida = new Date(horaSalida); 
     }
-    mostrar(){
 
+    mostrarFechas(){
     return this.Entrada.getTime() === this.Salida.getTime()? "Error, no se puede ingresar fechas iguales": 
     this.Salida.getTime() < this.Entrada.getTime()? "Error, la hora de salida no puede ser anterior a la de entrada" : 
     "De " + this.Entrada.toLocaleString('es-BO', { hour12: false }) + " hasta " + this.Salida.toLocaleString('es-BO', { hour12: false }); 
+    }
+    calcular(){
+        return 10; 
     }
 }
--- src/ticketParking.spec.js
+++ src/ticketParking.spec.js
@@ -6,7 +6,7 @@ describe("TicketParking", () => {
         let test = "2025-09-09T10:30";
         let test2 = "2025-09-09T10:40";
         let ticket = new ticketParking(test,test2); 
-        expect(ticket.mostrar()).toEqual("De 9/9/2025, 10:30:00 hasta 9/9/2025, 10:40:00"); 
+        expect(ticket.mostrarFechas()).toEqual("De 9/9/2025, 10:30:00 hasta 9/9/2025, 10:40:00"); 
     });
 
     //Registrar hora de ingreso y Registrar hora de salida 
@@ -14,14 +14,14 @@ describe("TicketParking", () => {
         let test = "2025-09-09T10:30";
         let test2 = "2025-09-09T10:50";
         let ticket = new ticketParking(test,test2); 
-        expect(ticket.mostrar()).toEqual("De 9/9/2025, 10:30:00 hasta 9/9/2025, 10:50:00");  
+        expect(ticket.mostrarFechas()).toEqual("De 9/9/2025, 10:30:00 hasta 9/9/2025, 10:50:00");  
     }); 
 
     // **validar que la hora entrada y salida no sean la misma
     it("al ingresar datos similares, lanzar un mensaje de error", () => {
         let test = "2025-09-09T10:30";
         let ticket = new ticketParking(test,test); 
-        expect(ticket.mostrar()).toEqual("Error, no se puede ingresar fechas iguales"); 
+        expect(ticket.mostrarFechas()).toEqual("Error, no se puede ingresar fechas iguales"); 
     });
 
     // **Validar que la hora de salida no sea anterior a la hora de entrada 
@@ -29,7 +29,15 @@ describe("TicketParking", () => {
         let test = "2025-09-09T10:30";
         let test2 = "2025-09-08T10:10";
         let ticket = new ticketParking(test,test2); 
-        expect(ticket.mostrar()).toEqual("Error, la hora de salida no puede ser anterior a la de entrada")
+        expect(ticket.mostrarFechas()).toEqual("Error, la hora de salida no puede ser anterior a la de entrada")
     });
+
+  //Mostrar la tarifa base 10.00 Bs la hora
+    it("Se debe de calcular la tarifa basica de una hora", () => {
+        let test = "2025-09-09T10:30";
+        let test2 = "2025-09-08T11:30";
+        let ticket = new ticketParking(test,test2); 
+        expect(ticket.calcular()).toEqual(10); 
+    })
     
 });  
